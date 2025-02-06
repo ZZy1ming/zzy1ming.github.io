1ccd8d5b2e3450c3a5fc814fdcff717463c883da
Yiming zhang <yz145@iu.edu>
2025-02-06 12:56:14 -0500

    just feel like to push

---
+++ main.js
@@ -0,0 +1,7 @@
+// function changeLanguage(lang) {
+//     if (lang === "en") {
+//       window.location.href = "index_en.html"; // Load the English version
+//     } else if (lang === "zh") {
+//       window.location.href = "index_zh.html"; // Load the Chinese version
+//     }
+//   }
